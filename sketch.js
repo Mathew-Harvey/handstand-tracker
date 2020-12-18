47a51bc3ae5253f9c8f153b2e0289ea4f437d65e
Mat <mathewharvey@gmail.com>
2020-12-18 13:49:49 +0800

    tracks skeleton up not upside down

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let video;
-let poseNet
-let pose
+let poseNet;
+let pose;
+let skeleton;
+
 function setup() {
   createCanvas(640, 400);
   video = createCapture(VIDEO);
@@ -13,6 +15,7 @@ function gotPoses(poses) {
   console.log(poses);
   if (poses.length > 0) {
     pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
   }
 }
 
@@ -23,9 +26,30 @@ function modelLoaded() {
 function draw() {
   image(video, 0, 0);
   if (pose) {
+
+    let eyeR =  pose.rightEye;
+    let eyeL =  pose.leftEye;
+    let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
+
     fill(255, 0, 0);
-    ellipse(pose.nose.x, pose.nose.y, 64)
+    ellipse(pose.nose.x, pose.nose.y, d)
     ellipse(pose.leftWrist.x, pose.leftWrist.y, 64)
     ellipse(pose.rightWrist.x, pose.rightWrist.y, 64)
+
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      fill (0,255,0);
+      ellipse(x,y, 16, 16);
+    }
+
+    for (let i = 0; i < skeleton.length; i++) {
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];
+      strokeWeight(2);
+      stroke(255);
+      line(a.position.x, a.position.y,b.position.x, b.position.y);
+      
+    }
   }
 }
