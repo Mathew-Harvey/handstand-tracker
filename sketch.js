b8e636c358380c478a12a65f58affa3c59a03759
Mat <mathewharvey@gmail.com>
2020-12-18 13:05:08 +0800

    Set up motion tracking on hands and nose

--- sketch.js
+++ sketch.js
@@ -1,19 +1,31 @@
 let video;
 let poseNet
+let pose
 function setup() {
   createCanvas(640, 400);
   video = createCapture(VIDEO);
   video.hide();
   poseNet = ml5.poseNet(video, modelLoaded)
+  poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded(){
+function gotPoses(poses) {
+  console.log(poses);
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+  }
+}
+
+function modelLoaded() {
   console.log('poseNet ready')
 }
 
 function draw() {
   image(video, 0, 0);
-  
-
+  if (pose) {
+    fill(255, 0, 0);
+    ellipse(pose.nose.x, pose.nose.y, 64)
+    ellipse(pose.leftWrist.x, pose.leftWrist.y, 64)
+    ellipse(pose.rightWrist.x, pose.rightWrist.y, 64)
+  }
 }
-
